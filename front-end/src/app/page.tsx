a741dc4849f7da58ac9e166f80e8fdd88a29554a
Fabio Andrade <fabio.adr12@gmail.com>
2025-02-19 08:25:46 -0300

    cria página de login

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,45 @@
 import styles from "./page.module.scss";
+import logoImg from "/public/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className={styles.teste}>TESTE TESTE</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        <Image
+          src={logoImg}
+          alt="Logo do app"
+        />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email"
+              className={styles.input}
+            />
+
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Digite sua senha"
+              className={styles.input}
+            />
+
+            <button type="submit" className={styles.button}>
+              Acessar
+            </button>
+          </form>
+
+          <Link href="/signup" className={styles.text}>
+            Cadastre-se
+          </Link>
+        </section>
+      </div>
+    </>
   );
 }
---
+++ front-end/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup(){
+    return(
+        <>
+            <main>
+                <h1>PAGINA CADASTRO</h1>
+            </main>
+        </>
+    )
+}
