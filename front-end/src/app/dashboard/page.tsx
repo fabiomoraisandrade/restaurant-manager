42b3dbc115e4c53afdcf315b167057d3b1078d58
Fábio Morais de Andrade <106340992+fabiomoraisandrade@users.noreply.github.com>
2025-02-19 11:57:29 +0000

    melhora identação e formatação em `signup/page.tsx` e `dashboard/page.tsx`

--- front-end/src/app/dashboard/page.tsx
+++ front-end/src/app/dashboard/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Dashboard() {
-    return(
-        <div>
-            <h1>Pagina Painel</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Pagina Painel</h1>
+    </div>
+  );
 }
--- front-end/src/app/signup/page.tsx
+++ front-end/src/app/signup/page.tsx
@@ -4,51 +4,48 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Signup() {
-    return(
-        <>
-            <div className={styles.containerCenter}>
-            <Image
-            src={logoImg}
-            alt="Logo do app"
+  return (
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo do app" />
+
+        <section className={styles.login}>
+          <h1>Criando sua conta</h1>
+          <form>
+            <input
+              type="text"
+              required
+              name="name"
+              placeholder="Digite seu nome"
+              className={styles.input}
             />
 
-            <section className={styles.login}>
-            <h1>Criando sua conta</h1>
-            <form>
-                <input
-                type="text"
-                required
-                name="name"
-                placeholder="Digite seu nome"
-                className={styles.input}
-                />
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email"
+              className={styles.input}
+            />
 
-                <input
-                type="email"
-                required
-                name="email"
-                placeholder="Digite seu email"
-                className={styles.input}
-                />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Digite sua senha"
+              className={styles.input}
+            />
 
-                <input
-                type="password"
-                required
-                name="password"
-                placeholder="Digite sua senha"
-                className={styles.input}
-                />
+            <button type="submit" className={styles.button}>
+              Cadastrar
+            </button>
+          </form>
 
-                <button type="submit" className={styles.button}>
-                Cadastrar
-                </button>
-            </form>
-
-            <Link href="/" className={styles.text}>
-                Já possui uma conta? Faça o login.
-            </Link>
-            </section>
-            </div>
-        </>
-    )
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Faça o login.
+          </Link>
+        </section>
+      </div>
+    </>
+  );
 }
