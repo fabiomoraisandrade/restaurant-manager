28140ad75c45bb46a24961a8c8510a84cc04b9ce
Fábio Morais de Andrade <106340992+fabiomoraisandrade@users.noreply.github.com>
2025-03-18 22:52:34 +0000

    Buscando e exibindo `orders`

--- front-end/src/app/dashboard/page.tsx
+++ front-end/src/app/dashboard/page.tsx
@@ -1,9 +1,30 @@
 import { Orders } from "./components/orders";
+import { api } from "@/services/api";
+import { getCookieServer } from "@/lib/cookieServer";
+import { OrderProps } from "@/lib/order.type";
 
-export default function Dashboard() {
+async function getOrders(): Promise<OrderProps[] | []> {
+  try {
+    const { token } = await getCookieServer();
+    const response = await api.get("/api/v1/order", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data || [];
+  } catch (err) {
+    console.error(`Erro ao buscar orders: ${err}`);
+    return [];
+  }
+}
+
+export default async function Dashboard() {
+  const orders = await getOrders();
+
   return (
     <>
-      <Orders />
+      <Orders orders={orders} />
     </>
   );
 }
