7d683ad41c49d1c5a4ddbd80b3009e957ef1d9f5
Fabio Andrade <fabio.adr12@gmail.com>
2025-03-27 21:22:45 -0300

    desenvolve tela de `login` no mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,15 @@
 import { Text, View, StatusBar } from "react-native";
-import SignIn from "@/src/pages/SignIn";
+import Routes from "@/src/routes";
 
 export default function Index() {
   return (
-    <View>
+    <>
       <StatusBar
         backgroundColor="#1d1d2e"
         barStyle="light-content"
         translucent={false}
       />
-      <SignIn />
-    </View>
+      <Routes/>
+    </>
   );
 }
