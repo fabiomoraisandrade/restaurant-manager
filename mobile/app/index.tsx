ea9ea035328d9f261c7e85b1925c7c5fd6fa0c9d
Fabio Andrade <fabio.adr12@gmail.com>
2025-03-29 17:49:25 -0300

    desenvolve funcionalidade de fazer login

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,18 @@
 import { Text, View, StatusBar } from "react-native";
 import Routes from "@/src/routes";
+import { AuthProvider } from "../src/contexts/AuthContext"
 
 export default function Index() {
   return (
     <>
-      <StatusBar
-        backgroundColor="#1d1d2e"
-        barStyle="light-content"
-        translucent={false}
-      />
-      <Routes/>
+      <AuthProvider>
+        <StatusBar
+          backgroundColor="#1d1d2e"
+          barStyle="light-content"
+          translucent={false}
+        />
+        <Routes/>
+      </AuthProvider>
     </>
   );
 }
