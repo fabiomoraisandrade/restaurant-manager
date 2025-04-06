0b39f001a5823cf612332c7d40495f94ff1c3821
Fabio Andrade <fabio.adr12@gmail.com>
2025-04-05 22:15:36 -0300

    altera logo `.sgv` para `.png` no `front-end`

--- front-end/src/app/signup/page.tsx
+++ front-end/src/app/signup/page.tsx
@@ -1,5 +1,5 @@
 import styles from "../page.module.scss";
-import logoImg from "/public/logo.svg";
+import logoImg from "/public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { api } from "@/services/api";
